5a26167dc377402a03e0992b8c6c86c65a07414d
OmerAkgull <143122832+OmerAkgull@users.noreply.github.com>
2024-02-15 21:37:20 +0300

    Set interval for fetch

--- src/components/layout/exchange/Exchange.tsx
+++ src/components/layout/exchange/Exchange.tsx
@@ -5,25 +5,49 @@ import { useState, useEffect } from "react";
 import "./customTableStyles.css";
 
 export default function Exchange() {
-
-    interface ColumnData {
-        rank:number,
-        name:string,
-        priceUsd:number,
-        changePercent24Hr:number,
-        marketCapUsd:number,
-        supply:number;
-    }
-
-
   const [dataSource, setDataSource] = useState<ColumnData[]>([]);
-  const [totalPages, setTotalPages] = useState<number| undefined>(undefined);
-  const [loading, setLoading] = useState(false);
+  const [totalPages, setTotalPages] = useState<number | undefined>(undefined);
+  // const [loading, setLoading] = useState(false);
+
+  async function fetchData() {
+    // setLoading(true);
+    const response = await fetch("https://api.coincap.io/v2/assets");
+    const data = await response.json();
+    setDataSource(data.data);
+    setTotalPages(data.data.length);
+    // setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchData();  
+    const interval = setInterval(() => fetchData(), 3000)
+    return () => clearInterval(interval)
+  }, []);
+
+  interface ColumnData {
+    rank: string;
+    name: string;
+    priceUsd: string;
+    changePercent24Hr: string;
+    marketCapUsd: string;
+    symbol: string;
+    supply: string;
+  }
 
   const columns: TableColumnProps<ColumnData>[] = [
     {
       title: "Rank",
       dataIndex: "rank",
+    },
+    {
+      title: "",
+      dataIndex: "symbol",
+      render: (symbol: string) => (
+        <img className="imgWidth"
+          alt={symbol}
+          src={`https://assets.coincap.io/assets/icons/${symbol.toLowerCase()}@2x.png`}
+        ></img>
+      ),
     },
     {
       title: "Name",
@@ -32,49 +56,48 @@ export default function Exchange() {
     {
       title: "Price",
       dataIndex: "priceUsd",
-      render: (price: number) => `$${price}`,
+      render: (price: string) => `$${Number(price).toFixed(2)}`,
     },
     {
       title: "24h Change",
       dataIndex: "changePercent24Hr",
-      render: (changePercent24Hr: number) => `%${changePercent24Hr}`,
-      responsive: ['sm'],
+      render: (changePercent24Hr: string) => {
+        // `%${Number(changePercent24Hr).toFixed(2)}`
+        console.log(changePercent24Hr)
+        if(changePercent24Hr.startsWith("-")) {
+          return <span style={{color: 'red'}}>{Number(changePercent24Hr).toFixed(2)}%</span>
+        } else {
+          return <span style={{color: 'green'}}>{Number(changePercent24Hr).toFixed(2)}%</span>
+        }
+      }
+        ,
+        width: "15%",
+      responsive: ["sm"],
     },
-    {
-      title: "Market Cap",
-      dataIndex: "marketCapUsd",
-      render: (marketCapUsd: number) => `$${marketCapUsd}`,
-      responsive: ['lg'],
-    },
-    {
-      title: "Supply",
-      dataIndex: "supply",
-      responsive: ['xxl'],
-    },
+    // {
+    //   title: "Market Cap",
+    //   dataIndex: "marketCapUsd",
+    //   render: (marketCapUsd: string) => `$${Number(marketCapUsd).toFixed(2)}`,
+    //   responsive: ["lg"],
+    // },
+    // {
+    //   title: "Supply",
+    //   dataIndex: "supply",
+    //   responsive: ["xxl"],
+    // },
   ];
-
-  async function fetchData() {
-    setLoading(true);
-    const response = await fetch("https://api.coincap.io/v2/assets");
-    const data = await response.json();
-    setDataSource(data.data);
-    setTotalPages(data.data.length);
-    setLoading(false);
-  }
-
-  useEffect(() => {
-    fetchData();
-  }, []);
 
   return (
     <>
-      <Container id="exchanges"
+      <Container
+        id="exchanges"
         className={`d-flex pt-5 flex-column justify-content-center align-items-center ${styles.gradientBg}`}
         fluid
       >
         <h1 className={styles.exchangeText}>Market Update</h1>
+        <div className="h-100 table-width">
         <Table
-          loading={loading}
+          // loading={loading}
           columns={columns}
           dataSource={dataSource}
           pagination={{
@@ -84,6 +107,7 @@ export default function Exchange() {
           }}
           className={`mt-5 ${styles.customTable}`}
         />
+        </div>
       </Container>
     </>
   );
